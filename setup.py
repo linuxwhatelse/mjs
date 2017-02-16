a5d00f65e8955c88022bb478169dd69e85ea354d
tadly <me@tadly.de>
2017-02-16 15:06:15 +0100

    added python 3.6 to classifiers

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     keywords='mapper json server mjs',
